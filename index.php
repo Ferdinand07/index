d08bebb237303e6fad82d57dc076038facbe7ff7
lape <109544472+Ferdinand07@users.noreply.github.com>
2022-07-20 20:23:45 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Inscription_en_ligne/ipnetp</title>
+</head>
+
+<body>
+	<?php
+	include_once("header.php");
+	?>
+	<?php
+	include_once("body.php");
+	?>
+	<?php
+	include_once("footer.php");
+	?>
+</body>
+</html>
